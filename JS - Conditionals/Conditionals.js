6d84bc4a95f7456ad2f36ad6294db6d182351e6d
addhyan94 <addhyant85@gmail.com>
2026-01-18 12:01:13 +0530

    Netflix Main File

---
+++ JS - Conditionals/Conditionals.js
@@ -0,0 +1,65 @@
+console.log("Hey I'm Addhyan ...  \n We learn Conditionals Properties In JS ... "); 
+
+// let number = 92;
+let number = 82;
+
+if (number>90) {
+    console.log("Your Number is Match")
+}
+else{
+    console.log("Your Number is Not Match")
+    console.log("Try Again.... ")
+    
+}
+
+// .................................................
+
+let snum = 5;
+
+number += snum;
+console.log(number)
+console.log(number + snum +" +")
+console.log(number - snum +" -")
+console.log(number * snum +" *")
+console.log(number / snum +" /")
+console.log(number % snum +" %")
+console.log(number ** snum +" power")
+
+// ...............................................................
+
+// let age = 19;
+let age = 5;
+// let age = 15;
+
+if (age>18) {
+    console.log("You are adult");
+}
+else if (age <5 ) {
+    console.log("you are a child ");
+    
+} else {
+    console.log("You are not adult");
+    
+}
+
+// termary operator ...........................
+
+// let a =5;
+let a =2;
+let b =3;
+let c = a>b ? (a+b) : (b-a) ;
+console.log(c);
+
+// ..........................................
+/*
+let c = a>b ? (a+b) : (b-a) ; 
+            translates to : 
+
+    if (a>b){
+        let c = a+b;
+    }
+    else {
+        let c = b-a;    
+    }
+       console.log(c); 
+*/
