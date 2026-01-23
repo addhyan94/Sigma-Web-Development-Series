a75ed8934b40a7758e8d546795bc3ff7a951fe39
addhyan94 <addhyant85@gmail.com>
2026-01-23 09:23:59 +0530

    Data Type in JS

---
+++ JS - Basic Tags , Variables, Data Types & Objects/JS Data Type.js
@@ -0,0 +1,35 @@
+            // primitive Data Type 
+console.log("Prototype Is Primitive..")
+
+// var jesa ha let hota hai but let gloublw or local dono me alag alag tarike se behve karta hai 
+// But VAR dono jage same jesa behave karta hai mtlb agar glovel me var ki value alg ot local me var ki value alag to jo last me var ki value hogi bo usko he lega uper bale ko ni 
+// leken let ke sath esa ni hota isliye Hamesha Let ka use karna chiye ......
+
+let a = "Tiwari Ji";
+let b = 21;
+let c= 50.563;
+
+const d = true;
+let e = undefined;
+let f = null;
+
+console.log(a, b, c, d, e, f)
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f)
+
+
+            // Object Data Type ...
+
+
+console.log("Prototype Is Object..")
+let o = {
+    "Student Name" : "Addhyan Tiwari",
+    "Student RollNumber" : 3
+}
+
+console.log(o);
+o.cource = "BCA (Hons..)";
+
+console.log(o);
+
+o.fee = 85000;
+console.log(o);
