3218149ef4f8ffc1a8d5267fe5c60def4733b8f7
addhyan94 <addhyant85@gmail.com>
2026-01-23 09:23:17 +0530

    Js Variables

---
+++ JS - Basic Tags , Variables, Data Types & Objects/JS - Variables.js
@@ -0,0 +1,13 @@
+console.log("Hey This is a JS Variables Tution Time ")
+
+var a = 5;
+var b = 10;
+var c = "Tiwari_ji";
+console.log("Code is Running ")
+console.log(a + b +5)
+console.log(c)
+
+
+// Type Pata karne ke liye konse type ak variable hai 
+
+console.log(typeof a,typeof b, typeof c)
