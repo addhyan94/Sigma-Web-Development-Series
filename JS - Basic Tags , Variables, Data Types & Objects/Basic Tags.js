fb108fa9622de3838c0be0b4f3269db8fe4efa08
addhyan94 <addhyant85@gmail.com>
2026-01-23 09:22:50 +0530

    Basic JS Tags

---
+++ JS - Basic Tags , Variables, Data Types & Objects/Basic Tags.js
@@ -0,0 +1,25 @@
+    // Alert ka use ek popup window ke liye use hot ahai jo loding ke time sabse phale show hota hai .... 
+        alert("Hello Alert Message ...\n Press Ok  ")
+
+        // console.log Kisi bhi cheez ko print karane ke liye hota hai Jese Printf ka use hota hai bese he ..........
+
+        console.log("Code is Running ") 
+
+        //  Var ek variable ko define ya banane ke liye use hota hai ....
+
+        var a= prompt("Enter your Password: ")
+
+        console.log("This is a password :" + a)
+
+        var isTrue = confirm(" Are you sure you Hit the Ok button and blast your computer") 
+        // If , else ek condition ke liye use hota hai ..... 
+        if (isTrue) {
+            console.log("Your Computer is Blast ")
+        }else{
+            console.log("Your Computer is Not Blast ")
+        }
+
+        alert("Press F12 button on Your computer \nClick Ok goto Next")
+        
+        console.log("Code is Running ")
+        alert(" And go to console And Check \nYour Computer is safe ???")
