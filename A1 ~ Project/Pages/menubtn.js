5f294305f89baffc56cd097bab5c940ac077b409
addhyan94 <addhyant85@gmail.com>
2026-01-24 13:28:06 +0530

    JS File

---
+++ A1 ~ Project/Pages/menubtn.js
@@ -0,0 +1,9 @@
+
+    const menuBtn = document.querySelector('.menu-btn');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    menuBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+        menuBtn.classList.toggle('open');
+    });
+
