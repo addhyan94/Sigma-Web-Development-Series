1221e456a379713ae51724ecd93a664b6eb2981b
addhyan94 <addhyant85@gmail.com>
2026-01-25 16:53:27 +0530

    Function

---
+++ JS - Functions & String/Functions.js
@@ -0,0 +1,32 @@
+function massage(name) {
+    console.log("Hello" + name +"how are u");
+    console.log("Hello" + name +"where are u");
+    console.log("Hello" + name +"are u good ????");
+}
+
+massage(" shubham ");
+massage(" Ramesh ");
+
+function math(a, b) {
+    console.log( a + b );
+    console.log( a - b );
+    console.log( a * b );
+    console.log( a / b );
+    return a + b;
+}
+
+show = math (6,5);
+
+console.log("The sum of these number is : ", show);
+
+            // Arrow Function..................
+const functions = (num)=>{
+    console.log("This is a arrow Function");
+    console.log("And Hear is passing arrow function number : ",num);
+}
+
+functions(23);
+functions(52);
+functions(56);
+
+ 
