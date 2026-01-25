0eb0d979d4168b07693de85b5331528df5794950
addhyan94 <addhyant85@gmail.com>
2026-01-25 16:51:19 +0530

    Filter

---
+++ JS - Array/Filter.js
@@ -0,0 +1,18 @@
+
+// Most UseFull Function....
+
+let array=[1,2,3,4,5,6,7,8,9,10]
+
+const gratertnfv = value=>{
+    if (value>5) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+console.log(array.filter(gratertnfv))//Jo 5 se bada hai boi filter hoke show hoga..
+
+// const gratertnfv = value? true : false ; //Kuch karne ki nazayej kosish 😂
