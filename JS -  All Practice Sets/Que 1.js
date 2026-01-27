ca93a3c8332248c386b1280f58ff750ade544926
addhyan94 <addhyant85@gmail.com>
2026-01-27 09:56:50 +0530

    Practice Set 1

---
+++ JS -  All Practice Sets/Que 1.js
@@ -0,0 +1,11 @@
+// Create a variable of tupe string and try to add a number to it.
+
+let number = 21;
+let string ="Hello "
+
+let add = string + number;
+console.log(add) 
+
+/* Output ~~
+    Hello21
+*/
