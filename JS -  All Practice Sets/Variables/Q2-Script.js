492f7b9b4376e180a5344748c0dd52b5335dc1ca
addhyan94 <addhyant85@gmail.com>
2026-04-09 19:22:24 +0530

    Variables Code Practice In JS

---
+++ JS -  All Practice Sets/Variables/Q2-Script.js
@@ -0,0 +1,5 @@
+// Question => Ek const banao jisme "JS Seekh Raha Hu" likha ho. Phir use console.log karo.
+
+console.log("Code 1st Line ....");
+const text = "JS seekh raha hu";
+console.log(text);
