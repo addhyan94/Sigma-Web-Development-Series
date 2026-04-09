3c7054c124ffe44f7559de17f7c612640090763e
addhyan94 <addhyant85@gmail.com>
2026-04-09 19:22:13 +0530

    Variables Code Practice In JS

---
+++ JS -  All Practice Sets/Variables/Q1-Script.js
@@ -0,0 +1,8 @@
+// Question => Ek variable banao jisme tera naam ho (let use karo), aur ek variable banao jisme teri age ho (const use karo). Dono ko console.log se print karo.
+
+console.log("Code ki 1st Line ..");
+let name = "Addhyan";
+const age = 19;
+
+console.log("My Name Is: " + name);
+console.log("My Age Is: " + age);
