0dd0e94d4b998c4b9759739b296a0e062f919256
addhyan94 <addhyant85@gmail.com>
2026-04-09 19:22:36 +0530

    Variables Code Practice In JS

---
+++ JS -  All Practice Sets/Variables/Q3-Script.js
@@ -0,0 +1,6 @@
+// Question => let se score = 10 banao, print karo. Phir score = 20 karo, phir print karo.
+console.log('Code 1st Line ....');
+let score =10;
+console.log(score);
+score = 20;
+console.log(score);
