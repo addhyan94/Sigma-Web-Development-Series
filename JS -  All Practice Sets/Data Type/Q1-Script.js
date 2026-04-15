f4e19dc4c27ac7437b610f8d14a260acc3ec9509
addhyan94 <addhyant85@gmail.com>
2026-04-15 18:48:40 +0530

    Data Type Practice Question...

---
+++ JS -  All Practice Sets/Data Type/Q1-Script.js
@@ -0,0 +1,8 @@
+// Question => Ek string, ek number, ek boolean variable banao. Teeno ka typeof console.log karo.
+
+let stn ='Hello!';
+let num = 4456;
+let bln = true;
+console.log(typeof stn);
+console.log(typeof num);
+console.log(typeof bln);
