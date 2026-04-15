da531b8b616b9f8c3c13cf1ffd0ca829a72168d8
addhyan94 <addhyant85@gmail.com>
2026-04-15 18:49:35 +0530

    Data Type Practice Question..

---
+++ JS -  All Practice Sets/Data Type/Q3-Script.js
@@ -0,0 +1,5 @@
+// Question =>5 fruits ki array banao. Array ki length aur pehla fruit print karo.
+console.log('Code 1st Line...');
+let arr =["apple","Banana","Mango","Orange","Papaya"];
+console.log(arr.length);
+console.log(arr[0]);
