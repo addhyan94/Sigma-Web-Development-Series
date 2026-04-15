bd984bee38d97c47cc9e99e6891d99333207f29e
addhyan94 <addhyant85@gmail.com>
2026-04-15 18:48:53 +0530

    Data Type Practice Question...

---
+++ JS -  All Practice Sets/Data Type/Q2-Script.js
@@ -0,0 +1,7 @@
+// Question => Apna object banao jisme name aur city ho. Dono properties print karo.
+console.log('Object Properties :::');
+let object ={
+    "Student Name": "Addhyan Tiwari",
+    "Student RollNo.": 3
+}
+console.log(object);
