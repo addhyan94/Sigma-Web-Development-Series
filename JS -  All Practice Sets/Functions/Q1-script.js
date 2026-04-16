ab733a668365635c215d6a401892a3b599bf3d85
addhyan94 <addhyant85@gmail.com>
2026-04-16 09:13:02 +0530

    Functions Practice Question..

---
+++ JS -  All Practice Sets/Functions/Q1-script.js
@@ -0,0 +1,10 @@
+// Question=>"greet" function banao jo naam le aur "Hello, [naam]!" return kare. Call karke print karo.
+console.log('Code1st Line...');
+
+function greet(name){
+    let result = console.log("Hello " + name+" !");
+    return result;
+}
+
+greet("Addhyan");
+greet("Shubh");
