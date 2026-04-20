6772fefeb60f805b8e69dbb4f1600f73da7510f2
addhyan94 <addhyant85@gmail.com>
2026-04-20 11:47:30 +0530

    Functions Practice Question..

---
+++ JS -  All Practice Sets/Functions/Q2-script.js
@@ -0,0 +1,10 @@
+// Question =>Arrow function "multiply" banao jo 2 numbers le. multiply(4, 5) print karo.
+console.log('Code 1st Line...');
+const multiply =(num1, num2)=>{
+    let result = console.log("Resulet Is: "+num1*num2);
+    return result;
+}
+
+multiply(2,2);
+multiply(4,2);
+multiply(2,6);
