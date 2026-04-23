ce81787ab8c77f1f9e40cfaea321020586e9963a
addhyan94 <addhyant85@gmail.com>
2026-04-23 16:31:46 +0530

    Loop Practice Question..

---
+++ JS -  All Practice Sets/Loop/Q2-script.js
@@ -0,0 +1,5 @@
+// Question => ["Monday","Tuesday","Wednesday"] array banao. forEach se har din print karo.
+
+console.log('Code 1st Line...');
+let days =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Seturday", "Sunday"];
+days.forEach(x=> console.log(x));
