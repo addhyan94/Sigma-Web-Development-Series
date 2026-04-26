d3652c8a8a491bb493651ccc4718bdab190dc6a0
addhyan94 <addhyant85@gmail.com>
2026-04-26 14:01:16 +0530

    Array Practice Question..

---
+++ JS -  All Practice Sets/Array/Q3-script.js
@@ -0,0 +1,5 @@
+// Question => ["apple","banana","mango","grape"] mein se "mango" .find() se dhundo aur print karo.
+console.log('Code 1st Line....');
+let ar = ["apple","banana","mango","grape"];
+const check = ar.find(n=>n=="mango");
+console.log(check);
