9960f30395d5218337f7171dfd3fb39459909ead
addhyan94 <addhyant85@gmail.com>
2026-04-26 14:01:01 +0530

    Array Practice Question..

---
+++ JS -  All Practice Sets/Array/Q2-script.js
@@ -0,0 +1,5 @@
+// Question => [1,2,3,4,5] ke har number ko 3 se multiply karo .map() se. Result print karo.
+console.log("Code 1st Line..");
+let num =[1,2,3,4,5];
+const check = num.map(n=>n*3)
+console.log(check);
